2d6f6bd8a43b45e5f28903b127d367d93835301e
Chandra922-code <chandrayadala1@gmail.com>
2025-07-24 19:53:56 +0530

    dashboard changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useState } from "react";
 import './App.css';
 import Main from "./components/main";
 import Parent from "./parentComp/Parent";
+import Dashboard from "./components/Dashboard";
 function App() {
   // BABEL will convert JSX TO VANILLA JS OR PLAIN JS---> THIS WILL UNDERSTOOD BY THE BROWSER.
   const [initialState, setInitialState] = useState("Seshu")
   const [value, setValue] = useState("")
   return (
     <div className="App">
+      <Dashboard/>
      <Main/>
     </div>
   );
